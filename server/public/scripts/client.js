23c129307037fe9c02263be899c9731a3bdaec50
Johnny Keple <Johnny.Keple@gmail.com>
2021-04-19 11:27:04 -0500

    working on ther client side

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -6,6 +6,43 @@ function onReady() {
     console.log('DOM ready');
 }
 
-function (params) {
+function addingJokes() {
+    let funnyJoke = {
+        whoseJoke: $('whoseJokeIn').val(),
+        jokeQuestion: $('questionIn').val(),
+        punchLine: $('punchlineIn').val()
+    }
+    console.log('testing funnyJoke', funnyJoke);
     
+    $.ajax({
+        method: 'POST',
+        url: '/joke',
+        data: funnyJoke
+    })
+        .then(function (response) {
+            console.log('adding joke');
+            //we add this part when we start our second ajax
+        })
+        .catch(function (error) {
+            console.log('checking for errors');
+            alart('sorry!')
+        })
+        //emptying after pressing add joke
+        $('whoseJokeIn').val('');
+        $('questionIn').val('');
+        $('punchlineIn').val('');
+}
+
+function gettingJoke() {
+    $.ajax({
+        method: 'GET',
+        url: '/joke'
+    })
+        .then(function (response) {
+            console.log('response from the server', response);
+            //add this from the render
+        })
+        .catch(function () {
+            
+        })
 }
